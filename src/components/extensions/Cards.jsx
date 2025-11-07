79129f6fbec27d8cf84c0d6748de28a7513c7812
marziajalili <astronomyfancy@gmail.com>
2025-11-07 09:42:33 +0430

    added the scroll animation!

--- src/components/extensions/Cards.jsx
+++ src/components/extensions/Cards.jsx
@@ -1,8 +1,45 @@
 import ToggleButton from "./ToggleButton";
 import RemoveButton from "./RemoveButton";
-import { useEffect, useState } from "react";
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+
+// ScrollTrigger Plugin
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+// SplitText Plugin
+import { SplitText } from "gsap/SplitText";
+gsap.registerPlugin(SplitText);
 
 const Cards = ({ darkMode, data, setInitialData, initialData, setData }) => {
+  // add some animations
+  const containerRef = useRef(null);
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const cards = containerRef.current.querySelectorAll(".card");
+
+    console.log(cards);
+    cards.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        { opacity: 0, y: 50 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.6,
+          ease: "power3.out",
+          delay: index * 0.1, // 👈 stagger effect
+          scrollTrigger: {
+            trigger: card,
+            start: "top 90%",
+            toggleActions: "play none none reset",
+          },
+        }
+      );
+    });
+  }, [data]);
+
   // generating cards...
   const cards = data.map(({ logo, name, description, isActive }) => (
     <li
@@ -11,7 +48,7 @@ const Cards = ({ darkMode, data, setInitialData, initialData, setData }) => {
         darkMode
           ? "bg-Neutral-700 border-Neutral-600"
           : "bg-white border-Neutral-200"
-      }  h-full p-5 rounded-3xl border shadow-lg`}
+      }  h-full p-5 rounded-3xl border shadow-lg card`}
     >
       <div className="flex flex-col h-full">
         {/* Ensure the flex direction is column */}
@@ -59,7 +96,10 @@ const Cards = ({ darkMode, data, setInitialData, initialData, setData }) => {
   ));
 
   return (
-    <ul className="grid md:grid-cols-2 lg:grid-cols-3  gap-4 w-full items-center">
+    <ul
+      ref={containerRef}
+      className="grid md:grid-cols-2 lg:grid-cols-3  gap-4 w-full items-center"
+    >
       {cards}
     </ul>
   );
